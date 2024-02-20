8140053892a08412c1f79d3c434638c08152e680
Aditya <adityaa0544@gmail.com>
2024-02-21 00:01:42 +0530

    removed sensitive info

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = "mongodb+srv://adityaa0544:darkknightoo544@cluster0.lxgmqqe.mongodb.net/?retryWrites=true&w=majority";
+const mongoDB = "";
 
 main().catch((err) => console.log(err));
 async function main() {
